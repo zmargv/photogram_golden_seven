9674145b650119970b115b416421129cbd617633
demo-student-123 <raghu@firstdraft.com>
2017-07-19 17:29:55 -0500

    Add the Golden Seven routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  
+  # CREATE
+  get("/photos/new", { :controller => "pictures", :action => "new_form" })
+  get("/create_photo", { :controller => "pictures", :action => "create_row" })
+
+  # READ
+  get("/photos", { :controller => "pictures", :action => "index" })
+  get("/photos/:an_id", { :controller => "pictures", :action => "show" })
+
+  # UPDATE
+  get("/photos/:some_id/edit", { :controller => "pictures", :action => "edit_form" })
+  get("/update_photo/:the_id", { :controller => "pictures", :action => "update_row" })
+
+  # DELETE
+  get("/delete_photo/:toast_id", { :controller => "pictures", :action => "destroy_row" })
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :admin_users, ActiveAdmin::Devise.config

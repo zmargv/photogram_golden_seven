d11eb468e535cecdb2aabff3b52760b3b8799ae3
Zuka <zmargvel@chicagobooth.edu>
2017-07-20 01:29:46 +0000

    solved Update

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -12,7 +12,7 @@ class PicturesController < ApplicationController
         render("pic_templates/create_row.html.erb");
     end
     def index
-        @list_photos = Photo.all;
+        @list_photos = Photo.all.order('photos.id DESC');
         render("pic_templates/index.html.erb");
     end
     def show
@@ -21,9 +21,16 @@ class PicturesController < ApplicationController
         render("pic_templates/show.html.erb");
     end
     def edit_form
+        @pic = Photo.find(params["an_id"]);
         render("pic_templates/edit_form.html.erb");
     end
     def update_row
+        p = Photo.find(params["some_id"]);
+        
+        p.source = params["the_source"];
+        p.caption = params["the_caption"];
+        
+        p.save;
         render("pic_templates/update_row.html.erb");
     end
     def destroy_row
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+  get("/", { :controller => "pictures", :action => "index" })
   # CREATE
   get("/photos/new", { :controller => "pictures", :action => "new_form" })
   get("/create_photo", { :controller => "pictures", :action => "create_row" })
